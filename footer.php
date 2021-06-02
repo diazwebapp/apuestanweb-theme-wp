1760efcdb4027f8aa6b80229b28b0f3a245b0af6
erickoficial69 <erickoficial69@gmail.com>
2021-06-02 01:20:09 -0400

    actualizando redireccion en login

--- footer.php
+++ footer.php
@@ -5,10 +5,7 @@ endif; ?>
 		<footer>
 		
 			<div>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium ab commodi dignissimos, saepe aliquam distinctio omnis laborum blanditiis cum deserunt?
-					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor, expedita! Sequi perspiciatis asperiores aspernatur ipsam facilis cupiditate eos, deleniti in quas voluptas rem, consequuntur sed. Recusandae iste ipsum rerum sequi porro, deserunt, incidunt dolores debitis quaerat excepturi vero veritatis dolor exercitationem nostrum numquam praesentium quis sed nihil ad temporibus blanditiis! Id minus sunt ea provident ullam omnis unde illum voluptate esse aperiam. Debitis id illo officiis magnam tenetur ipsa numquam suscipit, quidem doloremque repudiandae voluptatem quasi iure distinctio itaque laborum est placeat vel possimus, aspernatur fugiat laudantium. Natus, odio optio commodi vel iste dicta enim modi, ex est accusamus animi!
-				</p>
+				
 			</div>
 			
 		</footer><!-- #site-footer -->
