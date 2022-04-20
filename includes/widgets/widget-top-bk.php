83831f46c23aae2234cb066019218a7f4acb49dc
diazwebapp <diazwebapp@gmail.com>
2022-04-19 22:56:00 -0400

    geolocation en widget bookmaker

--- includes/widgets/widget-top-bk.php
+++ includes/widgets/widget-top-bk.php
@@ -44,7 +44,34 @@ class top_bk extends WP_Widget
                 $rating_ceil = ceil(carbon_get_post_meta(get_the_ID(), 'rating'));
                 $bonus = carbon_get_post_meta(get_the_ID(), 'bonus_sum_table') ? carbon_get_post_meta(get_the_ID(), 'bonus_sum_table') : 'n/a';
                 $ref = carbon_get_post_meta(get_the_ID(), 'ref');
-                echo '<div class="top_box">
+
+                $bk_countries = carbon_get_post_meta(get_the_ID(),'countries');
+                $location = json_decode(GEOLOCATION);
+                if($location->success == true and $bk_countries and count($bk_countries) > 0):
+                    foreach($bk_countries as $country):
+                        if($country['country_code'] == $location->country_code):
+                            echo '<div class="top_box">
+                                    <div class="d-flex align-items-center justify-content-between">
+                                        <div class="top_serial">
+                                            <span class="serial">'.$key.'</span>
+                                            <img src="'.$image_png.'" class="img-fluid" alt="">
+                                        </div>
+                                        <div class="ratings">
+                                            <span>'.$rating_ceil.'</span>
+                                            <i class="fas fa-star"></i>
+                                        </div>
+                                    </div>
+                                    
+                                    <div class="btn_groups">
+                                        <a href="'.get_the_permalink().'" class="button">Revision</a>
+                                        <a href="'.$country['ref'].'" class="button">Apostar</a>
+                                    </div>
+                                </div>';
+                        endif;
+                    endforeach;
+                endif;
+                if(!$location->success):
+                    echo '<div class="top_box">
                         <div class="d-flex align-items-center justify-content-between">
                             <div class="top_serial">
                                 <span class="serial">'.$key.'</span>
@@ -61,6 +88,7 @@ class top_bk extends WP_Widget
                             <a href="'.$ref.'" class="button">Apostar</a>
                         </div>
                     </div>';
+                endif;
             endwhile;
             
             echo '</div> </div> </div>';
