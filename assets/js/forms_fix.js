4cf6b7b4506d8aaa52c6c3b08f4a762af5994217
diaz web app <diazwebapp@gmail.com>
2022-05-13 04:36:02 +0000

    editando desde gitpod

--- assets/js/forms_fix.js
+++ assets/js/forms_fix.js
@@ -59,10 +59,10 @@ window.addEventListener("load",()=>{
       const div_register_payments = register_form.querySelectorAll("div.iump-form-paybox")
       const register_form_divs = register_form.querySelectorAll("div")
       
-      
-        console.log(div_register_social)
-        console.log(div_register_payments)
-      
+      register_form_divs.forEach(div=>{
+        div.remove()
+      })
+      register_form.appendChild(div_register_payments)
     }
     /* form.innerHTML += `
           <div class="form-group input-group">
