01e0ff89e7dbbda65fe8f8fdd05b53a86c8b7c30
diaz web app <diazwebapp@gmail.com>
2022-05-27 23:07:53 -0400

    bug encontrado en function.php

--- functions.php
+++ functions.php
@@ -188,6 +188,7 @@ add_action('init', function(){
             endif;
             if(!is_wp_error( $response )):
                 $geolocation_resp =  wp_remote_retrieve_body( $response );
+                var_dump($geolocation_resp);
                 $geolocation["success"] = true;
                 $geolocation['country'] = $geolocation_resp['country'];
                 $geolocation['country_code'] = $geolocation_resp->country_code;
