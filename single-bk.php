3e010a8b5d685d8e3182b55722c44c7d7536c35c
diaz web app <diazwebapp@gmail.com>
2022-09-19 18:22:17 -0400

    mejorando ui en sigle-bk

--- single-bk.php
+++ single-bk.php
@@ -70,10 +70,10 @@ $bookmaker["logo"] = get_template_directory_uri() . '/assets/img/logo2.svg';
                             </div>
     
                             <div class="col-12 my-4 special-single-bk-button" >
-                                <b style="background:lightgray;font-size:1.7rem;border-radius:.5rem;" class="px-2 text-body text-uppercase" ><?php echo $bookmaker["bonus_slogan"] ?></b>
+                                <b style="background:lightgray;font-size:18px;border-radius:.5rem;" class="px-2 text-body text-uppercase" ><?php echo $bookmaker["bonus_slogan"] ?></b>
                             </div>
                             <div class="col-12 my-3 special-single-bk-button">                                
-                                <a href="<?php echo $bookmaker["ref_link"] ?>" class="text-uppercase badge badge-primary px-5" style="font-size:1.7rem;border-radius:.5rem;font-weight: 500" target="_blank"><?php echo _e("Visitar") ?> <i class="fa fa-external-link ml-5" aria-hidden="true"></i></a>                                    
+                                <a href="<?php echo $bookmaker["ref_link"] ?>" class="text-uppercase badge badge-primary px-5" style="font-size:15px;border-radius:.5rem;font-weight: 500" target="_blank"><?php echo _e("Visitar") ?> <i class="fa fa-external-link ml-5" aria-hidden="true"></i></a>                                    
                             </div>
                         </div>
                     </div>
