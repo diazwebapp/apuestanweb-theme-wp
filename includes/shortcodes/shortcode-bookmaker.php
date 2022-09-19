d1130d993131103729b04236c1e7412fa506f735
diaz web app <diazwebapp@gmail.com>
2022-09-18 20:28:14 -0400

    paginando shortcode [bookmaker] y detectando metodos de pago para su filtrado desde paginas

--- includes/shortcodes/shortcode-bookmaker.php
+++ includes/shortcodes/shortcode-bookmaker.php
@@ -2,17 +2,19 @@
 function shortcode_bookmaker($atts)
 {
     extract(shortcode_atts(array(
-        'num' => 10,
+        'num' => 2,
         'title' => false,
         'slogan' => false,
-        'model' => 1
+        'model' => 1,
+        'payment' => wp_get_post_terms(get_the_ID(), 'bookmaker-payment-methods', array('field' => 'slug')),
+        'paginate'=>false
     ), $atts));
     $ret = '';
     
     wp_reset_query();
 
     $args['post_type'] = 'bk';
-    $args['posts_per_page'] = $num;
+    $args['posts_per_page'] = -1;
     $args['order'] = 'DESC';
     $args['orderby'] = 'meta_value_num';
     $args['meta_key'] = '_rating';
@@ -22,12 +24,28 @@ function shortcode_bookmaker($atts)
         $new_bks = [];
         $location = json_decode(GEOLOCATION);
         $aw_system_country = aw_select_country(["country_code"=>$location->country_code]);
+        
         foreach ($query->posts as $bookmaker): 
             $exists = aw_detect_bookmaker_on_country($aw_system_country->id,$bookmaker->ID);
             if(isset($exists)):
                 $new_bks[] = $bookmaker;
             endif;
         endforeach;
+        //Elementos de la paginación
+        $path = $_SERVER["REQUEST_URI"];
+
+        $total_pages = ceil(count($new_bks) / $num);
+        $current_page = 1;
+        var_dump($_GET);
+        if(isset($_GET["page"])){
+            $current_page = intval($_GET["page"]) > $total_pages ? $total_pages :intval($_GET["page"]);
+        }
+        $cut = $current_page * $num -2;
+        if($cut > count($new_bks)){
+            $cut = $cut - count($new_bks)  ;
+            $cut = $cut * $current_page;
+        }
+        array_splice($new_bks,0, ($current_page == 1) ? $current_page -1 : $cut );
         
         if($model == 1): 
             $ret .=  "<div class='testimonial_area'>
@@ -48,10 +66,17 @@ function shortcode_bookmaker($atts)
         endif;
 
         if($model == 2):
+            
             foreach ($new_bks as $key_bk => $bookmaker):
-                $ret .= get_template_part("loop/bookmaker_list_{$model}",null,[
-                    'post'	=> $bookmaker,
-                ]);
+                if($current_page == 1 and  $key_bk  <= ($num - 1))
+                    $ret .= get_template_part("loop/bookmaker_list_{$model}",null,[
+                        'post'	=> $bookmaker,
+                    ]);
+                if($current_page > 1 and  $key_bk  <= ($num - 1))
+                    $ret .= get_template_part("loop/bookmaker_list_{$model}",null,[
+                        'post'	=> $bookmaker,
+                    ]);
+                
             endforeach;
         endif;
         
@@ -67,6 +92,20 @@ function shortcode_bookmaker($atts)
             $ret .=  "</div>
             </div>";
         endif;
+        if(count($new_bks) > 0 and $paginate=="yes"){
+            
+            $ret .= '
+            <div class="col-lg-12">
+                <div class="blog_pagination">
+                    <ul class="pagination_list">
+                        <span aria-current="page" class="page-numbers current">1</span>
+                        <a class="page-numbers" href="'.$path.'page/'.($current_page+1).'">2</a>
+                        <a class="next page-numbers" href="'.$path.'page/'.($current_page+1).'">&gt;</a>
+                    </ul>
+                </div>
+            </div>
+            ';
+        }
         ?>
         <script>
             document.addEventListener('DOMContentLoaded',()=>{
