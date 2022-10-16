77d3d5e51fbf1ae0f1ea8d4cb4373322feaa8204
diaz web app <diazwebapp@gmail.com>
2022-10-16 12:51:40 -0400

    bug en el convertidor de cuotas

--- includes/shortcodes/shortcode-predictions.php
+++ includes/shortcodes/shortcode-predictions.php
@@ -49,8 +49,9 @@ endif;
         if($predictions and count($predictions)> 0):
             
             foreach($predictions as $prediction):
-                var_dump("cuote->",$prediction['cuote']);
-                $oOddsConverter = new Converter($prediction['cuote'], 'eu');
+                $custom_cuote = str_replace(",",".",$prediction['cuote']);
+                var_dump("cuote->",$custom_cuote);
+                $oOddsConverter = new Converter($custom_cuote, 'eu');
                 $odds_result = $oOddsConverter->doConverting();
                 $prediction['cuote'] = $odds_result[$_SESSION['odds_format']];
 
