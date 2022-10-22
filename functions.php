6b7fcbce41dfd1c5a4959731c7664156596cad00
diaz web app <diazwebapp@gmail.com>
2022-10-21 20:43:17 -0400

    tareas 11, 12 completadas, de 14 faltan 5,6 y 10

--- functions.php
+++ functions.php
@@ -143,14 +143,14 @@ function enqueuing_admin_scripts(){
 add_action( 'admin_enqueue_scripts', 'enqueuing_admin_scripts' );
 
 
-    if ('disable_gutenberg') {
+    /* if ('disable_gutenberg') {
         add_filter('use_block_editor_for_post_type', '__return_false', 100);
         remove_action('wp_enqueue_scripts', 'wp_common_block_scripts_and_styles');
         add_action('admin_init', function () {
             remove_action('admin_notices', ['WP_Privacy_Policy_Content', 'notice']);
             add_action('edit_form_after_title', ['WP_Privacy_Policy_Content', 'notice']);
         });
-    }
+    } */
 
 function draw_rating($rating)
 {
