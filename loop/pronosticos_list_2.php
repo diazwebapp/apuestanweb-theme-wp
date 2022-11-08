406f5f6ff328c985fee3ddf87d3421178cc6823f
diaz web app <diazwebapp@gmail.com>
2022-11-08 14:59:22 -0400

    detectando variable session

--- loop/pronosticos_list_2.php
+++ loop/pronosticos_list_2.php
@@ -17,7 +17,7 @@ $bookmaker = json_encode([]);
 //SI EL PAIS ESTÁ CONFIGURADO
 
 echo "<pre>";
-var_dump($_SERVER);
+var_dump($geolocation);
 echo "</pre>";
 if(isset($aw_system_location)):
     //SI EL SHORTCODE ES USADO EN UNA PAGINA
