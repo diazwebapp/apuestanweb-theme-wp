89516aa464fa54afe0c60ed9afc7821ddd5d2988
diaz web app <diazwebapp@gmail.com>
2022-11-07 22:48:27 -0400

    bug geolocation

--- includes/handlers/geolocation.php
+++ includes/handlers/geolocation.php
@@ -85,8 +85,8 @@ function geolocation_api(){
     
     
     if(!isset($_SESSION["geolocation"])){
-        $geolocation["ip"] = strval($_SERVER["REMOTE_ADDR"]);
         var_dump($_SERVER["REMOTE_ADDR"]);
+        $geolocation["ip"] = strval($_SERVER["REMOTE_ADDR"]);
         if($geolocation["ip"] !== "127.0.0.1" and $geolocation["ip"] != "::1"):
             
             $data_location = select_geolocation_cache($geolocation["ip"]);
