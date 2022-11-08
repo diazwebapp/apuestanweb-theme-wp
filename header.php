c43d71dd1cfc813cd5076f9f6cd5f97f08b9ed85
diaz web app <diazwebapp@gmail.com>
2022-11-08 11:05:41 -0400

    continuando con las fallas

--- header.php
+++ header.php
@@ -9,6 +9,7 @@
 <body>
 <?php
     var_dump($_SERVER["REMOTE_ADDR"]);
+    geolocation_api();
     $post_type = get_post_type( );
     if($post_type == "bk" and is_single()):
         echo do_shortcode("[slide_forecasts model='2']");
