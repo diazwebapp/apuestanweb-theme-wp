372495e6f6aa02a7622e3481693943a48a0ba6f7
diaz web app <diazwebapp@gmail.com>
2022-11-07 23:24:13 -0400

    bug geolocation

--- includes/handlers/geolocation.php
+++ includes/handlers/geolocation.php
@@ -37,6 +37,96 @@ function insert_geolocation_cache($data){
 }
 
 function geolocation_api(){
+    $ip = false;$response = false;
+
+    $geolocation = [
+        "ip" => $ip,
+        "country" => "World Wide",
+        "country_code" => "WW",
+        "timezone" => "America/Caracas",
+        "flag_uri" => get_template_directory_uri( ) . "/assets/img/ww.png"
+    ];
     
+    if (isset($_SERVER["HTTP_CLIENT_IP"]))
+    {
+        define('IP',$_SERVER["HTTP_CLIENT_IP"]);
+    }
+    elseif (isset($_SERVER["HTTP_X_FORWARDED_FOR"]) and !defined('IP'))
+    {
+        define('IP',$_SERVER["HTTP_X_FORWARDED_FOR"]);
+    }
+    elseif (isset($_SERVER["HTTP_X_FORWARDED"]) and !defined('IP'))
+    {
+        define('IP',$_SERVER["HTTP_X_FORWARDED"]);
+    }
+    elseif (isset($_SERVER["HTTP_FORWARDED_FOR"]) and !defined('IP'))
+    {
+        define('IP',$_SERVER["HTTP_FORWARDED_FOR"]);
+    }
+    elseif (isset($_SERVER["HTTP_FORWARDED"]) and !defined('IP'))
+    {
+        define('IP',$_SERVER["HTTP_FORWARDED"]);
+    }
+    else
+    {
+        define('IP',$_SERVER["REMOTE_ADDR"]);
+    }
+
+    $geolocation["ip"] = IP;
+    
+    $geolocation_api = empty(carbon_get_theme_option('geolocation_api')) ?"ipwhois": carbon_get_theme_option('geolocation_api') ;
+    $geolocation_api_key = carbon_get_theme_option('geolocation_api_key') ;
+    
+    if(!isset($_SESSION["geolocation"])){
+    
+        var_dump($geolocation["ip"]);
+        if($geolocation["ip"] !== "127.0.0.1" and $geolocation["ip"] != "::1"):
+            
+            $data_location = select_geolocation_cache($geolocation["ip"]);
+            if(count($data_location) == 0):
+                if(empty($geolocation_api) or empty($geolocation_api_key) or $geolocation_api == 'ipwhois'):
+                    if(!empty($geolocation_api_key)):
+                        $response = wp_remote_get("http://ipwho.pro/bulk/{$geolocation["ip"]}?key=$geolocation_api_key",array('timeout'=>10));
+                    endif;
+                    if(empty($geolocation_api_key)):
+                        $response = wp_remote_get("http://ipwho.is/{$geolocation["ip"]}",array('timeout'=>10));
+                    endif;
+                    if(!is_wp_error( $response )):
+                        $geolocation_resp =  wp_remote_retrieve_body( $response );
+                        $geolocation_resp = json_decode($geolocation_resp);
+                        if(isset($geolocation_resp->country) and isset($geolocation_resp->flag->img)):
+                            $geolocation["country"] = $geolocation_resp->country;
+                            $geolocation["country_code"] = $geolocation_resp->country_code;
+                            $geolocation["timezone"] = $geolocation_resp->timezone->id;
+                            $geolocation["flag_uri"] = $geolocation_resp->flag->img;
+                            insert_geolocation_cache($geolocation);
+                        endif;
+                    endif;
+                endif;
+        
+                if($geolocation_api == 'abstractapi' and !empty($geolocation_api_key)):
+                    $response = wp_remote_get("https://ipgeolocation.abstractapi.com/v1/?api_key=$geolocation_api_key&ip_address={$geolocation["ip"]}",array('timeout'=>10));
+                    if(!is_wp_error( $response )):
+                        $geolocation_resp =  wp_remote_retrieve_body( $response );
+                        $geolocation_resp = json_decode($geolocation_resp);
+    
+                        if(isset($geolocation_resp->country) and isset($geolocation_resp->flag->svg)):
+                            $geolocation["country"] = $geolocation_resp->country;
+                            $geolocation["country_code"] = $geolocation_resp->country_code;
+                            $geolocation["timezone"] = $geolocation_resp->timezone->name;
+                            $geolocation["flag_uri"] = $geolocation_resp->flag->svg; 
+                            insert_geolocation_cache($geolocation);
+                        endif; 
+                        
+                    endif;
+                endif;
+            endif;
+        
+        endif;
+        
+        $geolocation = json_encode($geolocation);
+        
+        $_SESSION["geolocation"] = $geolocation;
+    }
     
 }
