7ef45b8ab42d61d4c15f1bcc418dc3033b242159
diaz web app <diazwebapp@gmail.com>
2022-11-07 23:21:38 -0400

    bug geolocation

--- functions.php
+++ functions.php
@@ -260,7 +260,7 @@ add_action('init', function(){
     $geolocation_api = empty(carbon_get_theme_option('geolocation_api')) ?"ipwhois": carbon_get_theme_option('geolocation_api') ;
     $geolocation_api_key = carbon_get_theme_option('geolocation_api_key') ;
     
-    if(empty($_SESSION["geolocation"])){
+    if(!isset($_SESSION["geolocation"])){
     
         var_dump($geolocation["ip"]);
         if($geolocation["ip"] !== "127.0.0.1" and $geolocation["ip"] != "::1"):
