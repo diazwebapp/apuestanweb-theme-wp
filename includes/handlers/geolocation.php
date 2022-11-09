93dc4e6f32f86eefa83bbc34fd8b6af3c7233b18
diaz web app <diazwebapp@gmail.com>
2022-11-08 20:26:16 -0400

    testeando alternativas

--- includes/handlers/geolocation.php
+++ includes/handlers/geolocation.php
@@ -62,7 +62,7 @@ function geolocation_api($param){
     
     if(!isset($_SESSION["geolocation"])){
         
-        if($geolocation["ip"] !== "127.0.0.1" and $geolocation["ip"] !== "::1" and $geolocation["ip"] !== "143.198.170.157"):
+        if($geolocation["ip"] !== "127.0.0.1" and $geolocation["ip"] !== "::1"):
             var_dump("la ip es ".$param. "en ". date("h:i:s a"));
             
             $data_location = select_geolocation_cache($geolocation["ip"]);
