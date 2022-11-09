f763683d82df5f77fade75910067bad17c340459
diaz web app <diazwebapp@gmail.com>
2022-11-08 20:11:57 -0400

    testeando alternativas

--- loop/pronosticos_list_2.php
+++ loop/pronosticos_list_2.php
@@ -19,7 +19,7 @@ $bookmaker = json_encode([]);
 $ip = aw_get_the_user_ip();
 echo "<pre>";
 var_dump($ip);
-var_dump($_SERVER);
+var_dump($_SESSION);
 echo "</pre>";
 if(isset($aw_system_location)):
     //SI EL SHORTCODE ES USADO EN UNA PAGINA
