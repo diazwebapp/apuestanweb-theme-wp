a66123d474c1bf4403a4b9d46b009ebaf46eedae
diaz web app <diazwebapp@gmail.com>
2022-11-08 20:51:35 -0400

    testeando alternativas

--- loop/pronosticos_list_2.php
+++ loop/pronosticos_list_2.php
@@ -18,7 +18,7 @@ $bookmaker = json_encode([]);
 
 $ip = aw_get_the_user_ip();
 echo "<pre>";
-echo do_shortcode( '[Show_IP title="ip del pluguin"]' );
+echo do_shortcode( '[Show_IP title="ip del plugin"]' );
 var_dump("forecasts ip es ".$ip. " en ". date("h:i:s a"));
 echo "</pre>";
 if(isset($aw_system_location)):
