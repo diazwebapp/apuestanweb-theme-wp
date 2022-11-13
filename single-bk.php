3604949de3c34f4c43ecc1a40cbeb7905d8f0a4b
diaz web app <diazwebapp@gmail.com>
2022-11-13 00:59:49 -0400

    actualizado views parley y estrellitas

--- single-bk.php
+++ single-bk.php
@@ -40,8 +40,9 @@ $bookmaker["logo"] = get_template_directory_uri() . '/assets/img/logo2.svg';
                             <span class="text-uppercase font-weight-500 mr-3" style="font-size:20px;color:#F9F9F9;" ><?php echo $bookmaker["rating"]?></span>
                             <?php 
                                 if(isset($bookmaker["rating"])):
-                                    for($i=0; $i<intval($bookmaker["rating"]);$i++):
-                                        echo '<i style="font-size:15px;" class="fa fa-star text-warning px-1 py-1 align-text-bottom" ></i>';
+
+                                    for($i=0; $i<4;$i++):
+                                        echo '<i style="font-size:15px;" class="fa fa-star '.($i <= intval($bookmaker["rating"]) ? "text-warning" : "").' px-1 py-1 align-text-bottom" ></i>';
                                     endfor;
                                 endif;
                             ?>
