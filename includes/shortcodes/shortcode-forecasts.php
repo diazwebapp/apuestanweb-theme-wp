e112ea9675ec79024ce48d6c75d8a5c522d9a428
diaz web app <diazwebapp@gmail.com>
2022-11-13 00:09:18 -0400

    actualizado views forecasts y vip

--- includes/shortcodes/shortcode-forecasts.php
+++ includes/shortcodes/shortcode-forecasts.php
@@ -17,6 +17,7 @@ function shortcode_forecast($atts)
     $ret = "";
 
     $geolocation = json_decode($_SESSION["geolocation"]);
+    var_dump($geolocation->country_code);
 
     if(is_page() && !$title)
         $title = get_the_title( );
