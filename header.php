c2a58e781c5ab8784a25f14137c87e45781508fd
Andjar2 <andersonjjar@gmail.com>
2022-11-15 22:59:36 -0400

    up002

--- header.php
+++ header.php
@@ -3,12 +3,11 @@
 <head>
     <title><?php wp_title(''); ?></title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-	<meta name="viewport" content="width=device-width, initial-scale=1.001, user-scalable=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 	<?php wp_head(); ?>
 </head>
 <body>
 <?php
-
     $post_type = get_post_type( );
     if($post_type == "bk" and is_single()):
         echo do_shortcode("[slide_forecasts model='2']");
@@ -38,15 +37,10 @@
                     <a href="#" class="icon_box mr_10">
                         <img src="<?php echo get_template_directory_uri() . '/assets/img/icon8.svg'?>" alt="">
                     </a>
-                    <a href="<?php echo PERMALINK_VIP ?>" class="headerbtn"><?php $loc = json_decode($_SESSION["geolocation"]); echo $loc->country; ?> </a>
+                    <a href="<?php echo PERMALINK_VIP ?>" class="headerbtn"><?php $loc = json_decode(GEOLOCATION); echo $loc->country; ?> </a>
                     <a href="#" class="headerbtn v2">LOGIN</a>
-                    <div class="event_select">
-                        <select name="apu" id="apu">
-                                <option data-display="ES"></option>
-                                <option value="0">ES</option>
-                                <option value="apu">ES</option>
-                        </select>
-                    </div>
+
+                    
                 </div>
                 <div class="col-lg-8 order-lg-2">
                     <!--menu start-->
