23c9c44d1f3cf52f2a828575d4d0f59a3a0f851e
diaz web app <diazwebapp@gmail.com>
2022-11-18 13:03:11 -0400

    ._.

--- functions.php
+++ functions.php
@@ -222,7 +222,7 @@ add_action('init', function(){
         /* echo "<pre>";
         var_dump($_SERVER["REMOTE_ADDR"]);
         echo "</pre>"; */
-        $data = geolocation_api($_SERVER["HTTP_CF_CONNECTING_IP"]);
+        $data = geolocation_api($_SERVER["REMOTE_ADDR"]);
         $_SESSION["geolocation"] = json_encode($data);
     }
     
