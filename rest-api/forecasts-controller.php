fe6d753a7bbc9e5ebf68a8bc17feeedea17b9259
diaz web app <diazwebapp@gmail.com>
2022-11-17 23:08:24 -0400

    modificado forecasts

--- rest-api/forecasts-controller.php
+++ rest-api/forecasts-controller.php
@@ -55,8 +55,7 @@ function aw_get_forecasts(WP_REST_Request $request){
                     "country_code"=>isset($params['country_code']) ? $params['country_code'] : null,
                     "timezone" => isset($params['timezone']) ? $params['timezone'] : null
                 ]);
-            endif;
-            if(!isset($params["exclude_post"])):
+            else:
                 $loop_html .= load_template_part("loop/pronosticos_list_{$params['model']}",null,[
                     "forecast"=>$forecast,
                     "country_code"=>isset($params['country_code']) ? $params['country_code'] : null,
