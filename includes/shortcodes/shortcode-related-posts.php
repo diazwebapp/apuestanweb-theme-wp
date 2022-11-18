ecf648d100589bda3d7d619d5ab0c28886da923b
diaz web app <diazwebapp@gmail.com>
2022-11-17 21:27:11 -0400

    modificado forecasts

--- includes/shortcodes/shortcode-related-posts.php
+++ includes/shortcodes/shortcode-related-posts.php
@@ -38,7 +38,7 @@ function shortcode_news($atts)
     $args['post_type']      = 'post';
     $args['posts_per_page'] = $num;
     if(is_single() or is_singular()):
-        var_dump("singular => ").get_the_ID();
+        var_dump("singular => ".get_the_ID());
         $args['post__not_in']   = [get_the_ID()];
     endif;
     $league_arr=[];
