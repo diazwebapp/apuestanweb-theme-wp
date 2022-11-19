571ea9af4fac0d00c37c9c710bfb4f98f54034ca
diaz web app <diazwebapp@gmail.com>
2022-11-19 09:31:35 -0400

    default hoy en shortcode forecasts, vip y parley

--- includes/shortcodes/shortcode-forecasts-vip.php
+++ includes/shortcodes/shortcode-forecasts-vip.php
@@ -4,7 +4,7 @@ function shortcode_forecast_vip($atts)
     extract(shortcode_atts(array(
         'num' => 6,
         'league' => wp_get_post_terms(get_the_ID(), 'league', array('field' => 'slug')),
-        'date' => null,
+        'date' => "hoy",
         'model' => 1,
         'unlock' => null,
         'title' => null,
