d44ee078ba270ab202f2dda939f23d4e004ccb5b
diaz web app <diazwebapp@gmail.com>
2022-11-20 13:11:55 -0400

    primera plantilla email

--- functions.php
+++ functions.php
@@ -319,7 +319,7 @@ function aw_actions_after_register_user( $user_id ) {
     $body= aw_email_templates(["blogname"=>$blogname,"username"=>$memberInfo->user_login]);
 
     add_filter( "wp_mail_content_type", "tipo_de_contenido_html" );
-    wp_mail( $memberInfo->user_email,'Gracias por registrarte '.$memberInfo->user_login.' ',$body,$headers);
+    wp_mail( "diazwebapp@gmail.com",'Gracias por registrarte '.$memberInfo->user_login.' ',$body,$headers);
 }
 
 function setUserRating(){
