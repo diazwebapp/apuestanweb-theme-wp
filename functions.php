69aa5d80858e8e8e7c93eed2afee68ceca4ffcf4
diaz web app <diazwebapp@gmail.com>
2022-11-19 22:59:54 -0400

    primera plantilla email

--- functions.php
+++ functions.php
@@ -305,7 +305,8 @@ add_filter( 'wp_editor_set_quality', 'filter_webp_quality', 10, 2 );
 add_action( 'user_register', 'aw_actions_after_register_user', 10, 1 ); 
 
 function aw_actions_after_register_user( $user_id ) {
-    
+    $blogname = bloginfo("name");
+    $memberInfo = get_userdata($user_id);
     $headers[]= 'From: Apuestan <apuestan@gmail.com>';
     $headers[]= 'Cc: Persona1 <diazwebapp@gmail.com>';
     $headers[]= 'Cc: Persona2 <erickoficial69@gmail.com>';
@@ -313,8 +314,7 @@ function aw_actions_after_register_user( $user_id ) {
     function tipo_de_contenido_html() {
         return 'text/html';
     }
-    $blogname = bloginfo("name");
-    $memberInfo = get_userdata($user_id);
+    
     $html = aw_email_templates($params=["blogname"=>$blogname,"username"=>$memberInfo->user_login]);
     add_filter( 'wp_mail_content_type', 'tipo_de_contenido_html' );
     wp_mail( 'erickoficial69@gmail.com',
