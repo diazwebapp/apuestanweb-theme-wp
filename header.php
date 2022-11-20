7dabb3164a06b474dd26e09ad46cb8f7d9801657
diaz web app <diazwebapp@gmail.com>
2022-11-20 12:52:01 -0400

    primera plantilla email

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
     $memberInfo = get_userdata(29);
     $blogname = get_bloginfo( "name" );
     $admin_email = get_option( "admin_email" );
-    //var_dump($admin_email,$blogname,$memberInfo->user_email);
+    var_dump($admin_email,$blogname,$memberInfo->user_email);
 ?>
 <header class="sticky-top">
 <div class="container">
