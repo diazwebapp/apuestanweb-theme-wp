cd85487d7679f6f27070946cf90ec5c81c4cc866
diaz web app <diazwebapp@gmail.com>
2022-11-21 08:51:01 -0400

    solucionado odds converter

--- assets/js/common.js
+++ assets/js/common.js
@@ -26,7 +26,7 @@ let date_items = document.querySelectorAll('.date_item_pronostico_top');
             
             const request = await fetch(forecasts_fetch_vars.rest_uri+params)
             const response = await request.json()
-            console.log("max-pages= "+response.max_pages,"current-page= "+forecasts_fetch_vars.paged)
+            
             if(response.max_pages == forecasts_fetch_vars.paged){
                 e.target.remove()
             }
