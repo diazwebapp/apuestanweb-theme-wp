aafe1cfcb4eecc4aada66fecc4ff57331add20c0
diaz web app <diazwebapp@gmail.com>
2022-11-22 22:52:23 -0400

    odds fixed

--- assets/js/common.js
+++ assets/js/common.js
@@ -158,6 +158,7 @@ async function load_more_items(e){
     params += forecasts_fetch_vars.timezone ? "&timezone="+forecasts_fetch_vars.timezone:"";
     params += forecasts_fetch_vars.text_vip_link ? "&text_vip_link="+forecasts_fetch_vars.text_vip_link:"";
     params += forecasts_fetch_vars.unlock ? "&unlock="+forecasts_fetch_vars.unlock:"";
+    params += "&odds="+forecasts_fetch_vars.odds;
     
     const request = await fetch(forecasts_fetch_vars.rest_uri+params)
     const response = await request.json()
@@ -189,9 +190,11 @@ async function filter_date_items(e){
     params += forecasts_fetch_vars.timezone ? "&timezone="+forecasts_fetch_vars.timezone:"";
     params += forecasts_fetch_vars.text_vip_link ? "&text_vip_link="+forecasts_fetch_vars.text_vip_link:"";
     params += forecasts_fetch_vars.unlock ? "&unlock="+forecasts_fetch_vars.unlock:"";
+    params += "&odds="+forecasts_fetch_vars.odds;
+    
     const request = await fetch(forecasts_fetch_vars.rest_uri+params)
     const response = await request.json()
-    console.log(params)
+    
     let class_item =  e.getAttribute('data-type') 
     const div_container_pagination_forecasts = document.querySelector('.container_pagination_'+class_item)
     if(response.status == 'ok'){
