1dcc50a6b2982bef7931ebcea4c7d0212ca91c71
diaz web app <diazwebapp@gmail.com>
2022-11-23 20:08:14 -0400

    premiun bug

--- rest-api/forecasts-controller.php
+++ rest-api/forecasts-controller.php
@@ -101,8 +101,8 @@ function aw_get_forecasts_vip(WP_REST_Request $request){
     $args['meta_query']     = [
         [
             'key' => 'vip',
-            'value' => 'true',
-            'compare' => '=='
+            'value' => true,
+            'type' => 'BOOLEAN'
         ]
     ];
 
