9e9355b02195b7a519d4813bbae0824b05ff0450
diaz web app <diazwebapp@gmail.com>
2022-11-28 22:39:10 -0400

    testeando nav

--- header.php
+++ header.php
@@ -73,7 +73,7 @@
             </div>
         </div>
 </header>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
@@ -93,5 +93,5 @@
     </ul>
     
   </div>
-</nav>
+
 <?php
