76dc86715e147957d25394dbad572be33d41bf56
diaz web app <diazwebapp@gmail.com>
2022-11-29 10:13:46 -0400

    payment search params

--- includes/core/payment-dashboard/payment-history/payment-history-html.php
+++ includes/core/payment-dashboard/payment-history/payment-history-html.php
@@ -3,16 +3,18 @@
 function generate_history_payment_table(){
   global $wpdb;
   $params["status"] = "completed";
-  
+  if(isset($_GET["status"])){
+    if(isset($_GET["status"])){
+      $params["status"] = $_POST["status"];
+    }
+  }
   if(isset($_POST["filter_table_history"])){
     
     if(isset($_POST["username"]) and $_POST["username"] != ""){
       $params["username"] = $_POST["username"];
     }
     
-    if(isset($_POST["status"]) and $_POST["status"] != ""){
-      $params["status"] = $_POST["status"];
-    }
+    
     /*
     if(isset($_POST["date_1"]) or isset($_POST["date_2"])){
 
@@ -114,8 +116,21 @@ if(isset($_GET["update_payment_history_id"]) and isset($_GET["status"])){
 }
 function aw_payment_history(){
   $table_payment_history = generate_history_payment_table();
-  
+  $payment_method_navbar = '<nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top ">
+                              <div class="container-fluid">
+                                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                                  <span class="navbar-toggler-icon"></span>
+                                </button>
+                                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                                  <ul class="navbar-nav me-auto">
+                                    <li class="nav-item"><a class="nav-link" href="&status=pending">pending</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="&status=completed">completed</a></li>
+                                  </ul>
+                                </div>
+                              </div>
+                            </nav>';
     $dashboard = '<main class="container-fluid">
+    '.$payment_method_navbar.'
               <h4>'._x("payment history","jbetting").'</h4>
                 <div class="col-md-12 mb-2">
                       <h3>Filtrar consulta</h3>
