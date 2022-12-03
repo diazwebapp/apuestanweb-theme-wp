9fe914b411de85d7d39cfbb0e614a9937ecf8241
diaz web app <diazwebapp@gmail.com>
2022-12-03 10:04:21 -0400

    alineado el desplegable en header

--- header.php
+++ header.php
@@ -15,74 +15,74 @@
 ?>
 <header class="sticky-top">
 <div class="container">
-            <div class="row align-items-center form-row">
-                <div class="col-lg-2 order-lg-1 col-6 logo_col">
-                    <!-- menu toggler -->
-                    <div class="hamburger-menu">
-                        <span class="line-top"></span>
-                        <span class="line-center"></span>
-                        <span class="line-bottom"></span>
-                    </div>
-                    <!--logo start-->
-                    <?php if ( carbon_get_theme_option( 'logo' ) ):
-                            $logo = wp_get_attachment_url( carbon_get_theme_option( 'logo' ) );
-                        else:
-                            $logo =  get_template_directory_uri().'/assets/img/logo.svg';
-                        endif;
-                    ?>
-                    <a href="<?php echo get_home_url('/')?>" class="logo_box" ><img class="img-fluid" alt="apuestanweb logo" src="<?php echo $logo; ?>"></a>
-                    <!--logo end-->
-                    
-                </div>
-                <div class="col-lg-8 order-lg-2">
-                    <!--menu start-->
-                    <ul class="menu text-uppercase">                    
-                    <?php
-                        
-                        $ret = strip_tags( wp_nav_menu( array(
-                            'theme_location' => 'top',
-                            'echo'           => false
-                        ) ), '<li><a>' );
-                        if ( $ret ):
-                            echo $ret;
-                        else:
-                            echo "";
-                        endif;
-                        
-                    ?>
-                    </ul> <!--menu end-->
-                </div>
-                <div class="col-lg-2 order-lg-3 col-md-6">
-                        <?php 
-                            if(is_user_logged_in( )):
-                            echo '<div class="navbar navbar-expand-lg ">
-        
-                            <div class="navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav mr-auto ">
-                                
-                            <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                              Dropdown link
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                              <a class="dropdown-item" href="#">Action</a>
-                              <a class="dropdown-item" href="#">Another action</a>
-                              <a class="dropdown-item" href="#">Something else here</a>
-                            </div>
-                          </li>
-                                
-                            </ul>
-                            
-                            </div>
-                        </div>';
-                            else:
-                                echo '<a href="http://betmaste.com" class="btn_2 headerlgn mr-2"> Login</a>';
-                            endif;
-                        ?>
-    
-                    </div>
+    <div class="row align-items-center form-row">
+        <div class="col-6 logo_col order-lg-1 col-lg-2">
+            <!-- menu toggler -->
+            <div class="hamburger-menu">
+                <span class="line-top"></span>
+                <span class="line-center"></span>
+                <span class="line-bottom"></span>
             </div>
+            <!--logo start-->
+            <?php if ( carbon_get_theme_option( 'logo' ) ):
+                    $logo = wp_get_attachment_url( carbon_get_theme_option( 'logo' ) );
+                else:
+                    $logo =  get_template_directory_uri().'/assets/img/logo.svg';
+                endif;
+            ?>
+            <a href="<?php echo get_home_url('/')?>" class="logo_box" ><img class="img-fluid" alt="apuestanweb logo" src="<?php echo $logo; ?>"></a>
+            <!--logo end-->
+            
         </div>
+        <div class="col-6 order-lg-3 col-lg-2">
+                <?php 
+                    if(is_user_logged_in( )):
+                    echo '<div class="navbar navbar-expand-lg ">
+
+                    <div class="navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto ">
+                        
+                    <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-right" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Dropdown link
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                    </li>
+                        
+                    </ul>
+                    
+                    </div>
+                </div>';
+                    else:
+                        echo '<div class="text-right" ><a href="http://betmaste.com" class="btn_2 headerlgn mr-2"> Login</a></div>';
+                    endif;
+                ?>
+            </div>
+        <div class="col-6 col-lg-8 order-lg-2">
+            <!--menu start-->
+            <ul class="menu text-uppercase">                    
+            <?php
+                
+                $ret = strip_tags( wp_nav_menu( array(
+                    'theme_location' => 'top',
+                    'echo'           => false
+                ) ), '<li><a>' );
+                if ( $ret ):
+                    echo $ret;
+                else:
+                    echo "";
+                endif;
+                
+            ?>
+            </ul> <!--menu end-->
+        </div>
+        
+    </div>
+</div>
 </header>
 
 <?php
