8b1f68995b5fb2037897373285296ebbcba2d4a8
diaz web app <diazwebapp@gmail.com>
2022-12-04 16:07:40 -0400

    añadiendo login url

--- header.php
+++ header.php
@@ -47,9 +47,8 @@
                         Dropdown link
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <a class="dropdown-item" href="#">Something else here</a>
+                        <a class="dropdown-item" href="'. esc_url( !empty(get_option( 'ihc_general_user_page' )) ? get_the_permalink(get_option( 'ihc_general_user_page' )) :'/') .'">'._e('mi cuenta','jbetting').'</a>
+                        <a class="dropdown-item" href="'. esc_url( !empty(get_option( 'ihc_general_logout_page' )) ? get_the_permalink(get_option( 'ihc_general_logout_page' )) : wp_logout_url()) .'">'._e('mi cuenta','jbetting').'</a>
                     </div>
                     </li>
                         
