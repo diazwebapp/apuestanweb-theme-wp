37b42bbeae3bb51c66ec7f83774200ac17a67211
diaz web app <diazwebapp@gmail.com>
2022-12-05 10:33:50 -0400

    añadiendo login url

--- header.php
+++ header.php
@@ -43,7 +43,7 @@
                     <ul class="navbar-nav mr-auto">
                             
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle text-right text-light font-weight-bold" style="max-width:80px;" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <a class="nav-link dropdown-toggle text-right text-light font-weight-bold" style="max-width:80px !important;" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     '.get_userdata(get_current_user_id( ))->user_login .'
                                 </a>
 
