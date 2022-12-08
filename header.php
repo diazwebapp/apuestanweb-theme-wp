27757a20048a68a8c3914fb15cd01a038683cc81
diaz web app <diazwebapp@gmail.com>
2022-12-07 20:55:04 -0400

    retomamdo trabajo

--- header.php
+++ header.php
@@ -43,8 +43,8 @@
                     <ul class="navbar-nav mr-auto">
                             
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle text-right text-light font-weight-bold" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    '.get_userdata(get_current_user_id( ))->user_login .'
+                                <a class="nav-link dropdown-toggle text-light font-weight-bold" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <p class="d-inline text-truncate" style="max-width:70px;">'.get_userdata(get_current_user_id( ))->user_login .'</p>
                                 </a>
 
                                 <div class="dropdown-menu position-absolute text-center" style="right:0;font-size: 1.3rem;" aria-labelledby="navbarDropdownMenuLink">
