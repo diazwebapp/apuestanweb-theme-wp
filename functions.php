d79e1c9a1d7305320d396b48e8e2375cced29c11
diaz web app <diazwebapp@gmail.com>
2022-12-09 14:55:24 -0400

    retomamdo trabajo

--- functions.php
+++ functions.php
@@ -307,8 +307,8 @@ function aw_notificacion_membership($payment_history_id=null){
     }
     if(isset($payment_history_id)){
         $db = $wpdb->prefix . "aw_payment_history";
-        $data_notifi = $wpdb->get_row($wpdb->prepare("SELECT * FROM $db WHERE id='$payment_history_id'"));
-        var_dump($data_notifi->username );
+        $data_notifi = $wpdb->get_row($wpdb->prepare("SELECT * FROM ".MYSQL_PAYMENT_HISTORY." WHERE id='$payment_history_id'"));
+        var_dump( $data_notifi->username );
         /* $memberInfo = get_userdata($user_id);
         $blogname = get_bloginfo( "name" );
         $admin_email = get_option( "admin_email" );
