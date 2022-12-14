959cf9c06ec65732fb095b3f4414904722ee386c
diaz web app <diazwebapp@gmail.com>
2022-12-13 23:09:41 -0400

    :)

--- loop/banner_7.php
+++ loop/banner_7.php
@@ -2,21 +2,24 @@
 
 $params = get_query_var("params");
 $alt_logo = get_template_directory_uri() . '/assets/img/event-logo.png';
-$html_logo = "<img src='$alt_logo' class='logo_dark' alt='{$params['title']}' >";
+$text = !empty($params['text']) ? $params['text'] : 'CONVIERTE EN MIEMBRO PREMIUM';
+
+$html_logo = "<img src='$alt_logo' class='w-100' alt='{$params['title']}' >";
+$text_vip_link = !empty($params['text_vip_link']) ? $params['text_vip_link'] : 'VIP';
 if($params['src_logo']):
-    $html_logo = "<span class='{$params['src_logo']}' ></span>";
+    $html_logo = "<img src='{$params['src_logo']}' class='w-100' alt='{$params['title']}' >";
 endif;
 
 echo '<div class="vip_box v2">
 <div class="row align-items-center">
     <div class="col-md-9">
-        '.$html_logo.'
-        <h4>CONVIERTE EN MIEMBRO PREMIUM</h4>
-        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam no numy sit eirmod…</p>
-        <a href="'.$params['memberships_page'].'">Quiero ser VIP</a>
+        <img src='.$alt_logo.' class="logo_dark" alt='.$params['title'].' >
+        <h4>'.__("{$params['title']}","jbetting").'</h4>
+        <p>'.__("$text","jbetting").'</p>
+        <a href="'.$params['memberships_page'].'">'.__("$text_vip_link","jbetting").'</a>
     </div>
     <div class="col-md-3 d-none d-md-block">
-        <img src="img/value.png" class="w-100" alt="">
+        '.$html_logo.'
     </div>
 </div>
 </div>';
