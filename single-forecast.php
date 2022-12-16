280f27d74bd86c3123bd765e8bdad99d2cae6158
diaz web app <diazwebapp@gmail.com>
2022-12-15 21:08:55 -0400

    mejorando restriccion a contenido vip

--- single-forecast.php
+++ single-forecast.php
@@ -22,21 +22,17 @@
                             ////////// RESTRICTIONS
                             if (!empty($post_meta['ihc_mb_who'])){
                                 $post_restringido = true;
-                                $array_posts_lid = explode(",",$post_meta['ihc_mb_who']);
-                                var_dump($array_posts_lid);
                             }
 
-                            if(is_user_logged_in(  ) and !empty($vip)){
-                                $post_id = get_the_ID();
+                            if(is_user_logged_in()  and $post_restringido){
                                 $table = $wpdb->prefix."postmeta";
                                 $user_id = get_current_user_id(  );
                                 $user_levels = \Indeed\Ihc\UserSubscriptions::getAllForUserAsList( $user_id, true );
                                 $user_levels = apply_filters( 'ihc_public_get_user_levels', $user_levels, $user_id );
                                 
                                 if(!empty($user_levels)){
-                                    $data = $wpdb->get_var("SELECT meta_value FROM `$table` WHERE post_id='$post_id' AND meta_key = 'ihc_mb_who'");
-                                                                        
-                                    $array_posts_lid = explode(",",$data);
+
+                                    $array_posts_lid = explode(",",$post_meta['ihc_mb_who']);
                                     if(count($array_posts_lid) > 0){
 
                                         $usuario_permitido = in_array($user_levels,$array_posts_lid);  
@@ -160,9 +156,9 @@
                                         <?php 
 
                                         
-                                        if($usuario_permitido): echo do_shortcode("[predictions]"); endif; ?>		
+                                        if(!$post_restringido): echo do_shortcode("[predictions]"); endif; ?>		
                                         <?php the_content() ?>	
-                                        <?php if($usuario_permitido): echo do_shortcode("[predictions]"); endif; ?>		
+                                        <?php if(!$post_restringido): echo do_shortcode("[predictions]"); endif; ?>		
                                     </div>
                                     <?php echo do_shortcode("[user_stats]") ?>	
                                     <div class="title_wrap single_event_title_wrap">
