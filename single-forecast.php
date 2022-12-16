cdf983fb3eeb631b91bf91b5dbb5878fa0b17f89
diaz web app <diazwebapp@gmail.com>
2022-12-16 08:50:52 -0400

    odds test

--- single-forecast.php
+++ single-forecast.php
@@ -37,7 +37,6 @@ function aw_get_user_type(){
             <div class="row">
                 <?php 
                     if(have_posts()):
-                        var_dump(floatval(1));
                         $oOddsConverter = new Converter(1, 'eu');
                         $odds_result = $oOddsConverter->doConverting();
                         var_dump($odds_result);
