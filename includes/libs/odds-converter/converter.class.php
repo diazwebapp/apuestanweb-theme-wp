bb2632c0a8b38ae70f191b5a4d25ce02993a8421
diaz web app <diazwebapp@gmail.com>
2022-12-16 10:26:59 -0400

    odds test

--- includes/libs/odds-converter/converter.class.php
+++ includes/libs/odds-converter/converter.class.php
@@ -61,7 +61,7 @@ class Converter {
         //a > 0 ? a / 100 + 1 : 100 / a * -1 + 1
         $iDecimal -= 1;
         if($iDecimal < 1){
-            return "-" .  round(($iDecimal + 100/$iDecimal) * 10,2) ;
+            return "-" .  round(($iDecimal + 100/$iDecimal -1) * 10,2) ;
         }else{
             return "+" . round(($iDecimal + 100 * $iDecimal),3) ;
         }
