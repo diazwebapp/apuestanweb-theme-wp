ede33598f2bf1f7b175c245591608911e5ad3f2a
diaz web app <diazwebapp@gmail.com>
2022-12-16 12:55:17 -0400

    actualizando profile-forecaste

--- loop/profile-forecaster-1.php
+++ loop/profile-forecaster-1.php
@@ -16,8 +16,8 @@ if(isset($_GET['profile'])):
     $avatar_url = get_avatar_url($id_author);
     $avatar = isset($avatar_url) ? $avatar_url : get_template_directory_uri() . '/assets/img/logo2.svg';
     $total_forecast = $acerted + $failed;
-     /* $porcentage = $acerted * 100 / $total_forecast;
-    $stats_vip = get_user_stats($id_author,'=');
+    $porcentage = $acerted * 100 / $total_forecast;
+     /* $stats_vip = get_user_stats($id_author,'=');
     $stats_free = get_user_stats($id_author,'!=');
     //estadisticas ultimos 2 meses
     $num = 4;
