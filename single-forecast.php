bb369ddf15f210afc77016e8cc3f15f3a36b0fc1
diaz web app <diazwebapp@gmail.com>
2022-12-15 21:27:28 -0400

    mejorando restriccion a contenido vip

--- single-forecast.php
+++ single-forecast.php
@@ -49,8 +49,8 @@
                                     (isset($post_id)) ? $post_id : -1);
                                 
                                 var_dump($block);
-                                var_dump($current_user);
-                                var_dump($target_users);
+                                var_dump("current_user ",$current_user);
+                                var_dump("user_targets ", $target_users);
 
                                 /* $user_id = get_current_user_id(  );
                                 $user_levels = \Indeed\Ihc\UserSubscriptions::getAllForUserAsList( $user_id, true );
