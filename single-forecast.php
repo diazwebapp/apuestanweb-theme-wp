a7c8351ff6f3f9af2498b909604f184829bf384e
diaz web app <diazwebapp@gmail.com>
2022-12-20 12:23:11 -0400

    error single-forecast en local

--- single-forecast.php
+++ single-forecast.php
@@ -121,10 +121,10 @@ function aw_get_user_type(){
                                     $league->permalink = isset($league_page->ID) ? get_permalink($league_page->ID) : get_term_link($league, 'league');
                                 endif;
                             endif;
-
+                            var_dump($sport);
                             //forecast teams
                             $teams = get_forecast_teams(get_the_ID(),["w"=>50,"h"=>50]);
-                            var_dump($teams);
+                           
                             ?>
                             <div class="col-lg-8 mt_30">
                     
