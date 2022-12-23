9393882175e0f4a5c6241b771003c247dfaf6e6a
diaz web app <diazwebapp@gmail.com>
2022-12-23 09:18:09 -0400

    creo que ya

--- includes/templates-emails/template-email-2.php
+++ includes/templates-emails/template-email-2.php
@@ -2,6 +2,10 @@
 function aw_email_templates_2($params=["blogname"=>"","username"=>""]){
     $html = '<div>{vip_page} {blogname} {username}</div>';
     
+    
+    
+    $html = str_replace("{blogname}",$params["blogname"],$html);
+    $html = str_replace("{username}",$params["username"],$html);
 
     return $html;
 }
