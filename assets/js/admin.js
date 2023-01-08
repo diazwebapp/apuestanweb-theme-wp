52939a92fca72c039ef38eec9b3f17da192cbd7e
diaz web app <diazwebapp@gmail.com>
2023-01-07 21:05:15 -0400

    history details

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -218,6 +218,7 @@ async function modal_payment_details(button){
             if(res.metas && res.metas.length > 0){
                 let div = ''
                 for(metas of res.metas){
+                    console.log(metas)
                     div += `<div><b>${metas.key} :</b> ${metas.value}</div>`
                 }
                 modal_event(button,div)
