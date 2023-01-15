39c2c8d5f2640cdd23c6b428b9c26e3ccc8a9412
diaz web app <diazwebapp@gmail.com>
2023-01-15 15:22:02 -0400

    vip bug authors

--- loop/pronosticos_vip_list_1_unlock.php
+++ loop/pronosticos_vip_list_1_unlock.php
@@ -60,7 +60,7 @@ if ($sport_term) {
 
 if ($teams['team1']['logo'] and $teams['team2']['logo'] ):
     $author_id = get_the_ID();
-    var_dump($author_id);
+    var_dump($args["forecast"]->ID);
     var_dump(get_the_author_ID());
     //$author_id = get_post_field( 'post_author', get_the_ID() );
     $acerted = get_the_author_meta("forecast_acerted", $author_id );
