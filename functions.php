1995e63c3553d91ba0ccfd9dd5afdb1ff20d8c19
diaz web app <diazwebapp@gmail.com>
2023-01-18 14:32:40 -0400

    primer test emails

--- functions.php
+++ functions.php
@@ -352,12 +352,12 @@ function aw_notificacion_membership($payment_history_id=null,$status=null){
         if(isset($status)){
             if($status=="completed"){
                 $message = '
-                <p style="font-size: 14px; line-height: 140%;">Hellow {username}, Your account is approved.</p>';
+                <p style="font-size: 14px; line-height: 140%;">Hi {username}, Your account is approved.</p>';
                 $body= aw_email_templates(["blogname"=>$blogname,"username"=>$memberInfo->user_login,"vip_link"=>$vip_link,"message"=>$message]);
             }
             if($status=="pending"){
                 $message = '
-                <p style="font-size: 14px; line-height: 140%;">Hellow {username}, Your account is approved.</p>';
+                <p style="font-size: 14px; line-height: 140%;">Hi {username}, Your account is waiting to be approved.</p>';
                 $body= aw_email_templates(["blogname"=>$blogname,"username"=>$memberInfo->user_login,"vip_link"=>$vip_link,"message"=>$message]);
 
             }
