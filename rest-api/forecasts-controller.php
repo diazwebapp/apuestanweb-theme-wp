0e2fdc63e68f04745a884f95abed43a63c39d047
diaz web app <diazwebapp@gmail.com>
2023-01-30 14:13:18 -0400

    futbol

--- rest-api/forecasts-controller.php
+++ rest-api/forecasts-controller.php
@@ -142,14 +142,14 @@ function aw_get_forecasts_vip(WP_REST_Request $request){
             
             foreach ($query->posts as $key => $forecast):
                 if(isset($params["exclude_post"]) and $params["exclude_post"] != $forecast->ID):
-                    $loop_html["html"] .= load_template_part("loop/pronosticos_vip_list_{$params['model']}_unlock",null,[
+                    $loop_html["html"] .= load_template_part("loop/pronosticos_list_{$params['model']}",null,[
                     "forecast"=>$forecast,
                     "country_code"=>isset($params['country_code']) ? $params['country_code'] : null,
                     "timezone" => isset($params['timezone']) ? $params['timezone'] : null
                     ]); 
                 endif;
                 if(!isset($params["exclude_post"])):
-                    $loop_html["html"] .= load_template_part("loop/pronosticos_vip_list_{$params['model']}_unlock",null,[
+                    $loop_html["html"] .= load_template_part("loop/pronosticos_list_{$params['model']}",null,[
                     "forecast"=>$forecast,
                     "country_code"=>isset($params['country_code']) ? $params['country_code'] : null,
                     "timezone" => isset($params['timezone']) ? $params['timezone'] : null
@@ -161,14 +161,14 @@ function aw_get_forecasts_vip(WP_REST_Request $request){
             
             foreach ($query->posts as $key => $forecast):
                 if(isset($params["exclude_post"]) and $params["exclude_post"] != $forecast->ID):
-                    $loop_html["html"] .= load_template_part("loop/pronosticos_vip_list_{$params['model']}",null,[
+                    $loop_html["html"] .= load_template_part("loop/pronosticos_list_{$params['model']}",null,[
                     "forecast"=>$forecast,
                     "country_code"=>isset($params['country_code']) ? $params['country_code'] : null,
                     "timezone" => isset($params['timezone']) ? $params['timezone'] : null
                     ]); 
                 endif;
                 if(!isset($params["exclude_post"])):
-                    $loop_html["html"] .= load_template_part("loop/pronosticos_vip_list_{$params['model']}",null,[
+                    $loop_html["html"] .= load_template_part("loop/pronosticos_list_{$params['model']}",null,[
                     "forecast"=>$forecast,
                     "country_code"=>isset($params['country_code']) ? $params['country_code'] : null,
                     "timezone" => isset($params['timezone']) ? $params['timezone'] : null
