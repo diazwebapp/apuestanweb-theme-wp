973fb3448a45eb482a39024d10f530ca87cc7258
diaz web app <diazwebapp@gmail.com>
2023-01-31 09:52:44 -0400

    bug extraño

--- includes/core/notification-module/notification-core.php
+++ includes/core/notification-module/notification-core.php
@@ -35,6 +35,7 @@ if(!function_exists('select_notification_not_view')):
         $table_posts = $wpdb->prefix."posts";
         $current_user = ($setup['id_user'] ? $setup['id_user'] : get_current_user_id( ));
         $result = [];
+        ob_start();
         if(isset($current_user)):
             $sql = "SELECT * FROM $table_posts Where Not exists (select * from $table_notification Where $table_notification.id_pronostico = $table_posts.ID AND $table_notification.id_user = $current_user) ". ($setup['post_type'] ? " AND $table_posts.post_type = '{$setup['post_type']}' " :'' )." AND $table_posts.post_status='publish'";
 
@@ -51,6 +52,7 @@ if(!function_exists('select_notification_not_view')):
                 }
             endif;
         endif;
+        ob_end_clean();
         return $new_result;
     }
 endif;
