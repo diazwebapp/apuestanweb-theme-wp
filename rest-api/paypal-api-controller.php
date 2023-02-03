3671298207f420c00e4dfbe059e03500e051e841
diaz web app <diazwebapp@gmail.com>
2023-02-03 13:01:02 -0400

    modificando respuesta de la api paypal

--- rest-api/paypal-api-controller.php
+++ rest-api/paypal-api-controller.php
@@ -81,8 +81,8 @@ if(!function_exists("aw_paypal_capture_order")):
 
         $authorization = "Bearer ".$token_data->access_token;
         $paypal_api_uri = "https://api-m.paypal.com/v2/checkout/orders";
-        $paypal_api_uri = "https://api-m.sandbox.paypal.com/v2/checkout/orders";
         if($dev_mode): // si el tema esta de desarrollo se activa en modo "sandbox"
+            $paypal_api_uri = "https://api-m.sandbox.paypal.com/v2/checkout/orders";
         endif;
         $response = wp_remote_post( "$paypal_api_uri/{$params['token']}/capture", array(
             'method'      => 'POST',
