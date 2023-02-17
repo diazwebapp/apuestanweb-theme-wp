12fd03a78651b2a68731f8b781310610afacd006
diaz web app <diazwebapp@gmail.com>
2023-02-17 12:21:57 -0400

    creando modulo para generar imagen y aplicar destacada manualmente

--- rest-api/imagen-detacada-controller.php
+++ rest-api/imagen-detacada-controller.php
@@ -10,9 +10,10 @@ if(!function_exists('aw_imagen_destacada_controller')):
             $base_64 = str_replace("data:image/png;base64,","",$params["base64"]);
             $bin = base64_decode($base_64);
             $im = imagecreatefromstring($bin);
-            $wp_upload_dir = wp_upload_dir();
             $filename = $post->post_name.".png";
-            $ruta = $wp_upload_dir['url'] . "/" .$filename ;
+
+            $wp_upload_dir = wp_upload_dir();
+            $ruta = $wp_upload_dir['path'] . "/" .$filename ;
             imagepng($im,$ruta);
             aw_set_imagen_destacada($ruta,$post->ID);
         endif;
@@ -22,32 +23,28 @@ if(!function_exists('aw_imagen_destacada_controller')):
 else:
     echo "la funcion aw_imagen_destacada_controller ya existe";
 endif;
-function aw_set_imagen_destacada($filename,$parent_post_id){
+function aw_set_imagen_destacada($image_full_path,$parent_post_id){
 
     // Check the type of file. We'll use this as the 'post_mime_type'.
-    $filetype = wp_check_filetype( basename( $filename ), null );
-
-    // Get the path to the upload directory.
-    $wp_upload_dir = wp_upload_dir();
+    $filetype = wp_check_filetype( basename( $image_full_path ), null );
 
     // Prepare an array of post data for the attachment.
     $attachment = array(
-        'guid'           => $filename, 
+        'guid'           => $image_full_path, 
         'post_mime_type' => $filetype['type'],
-        'post_title'     => preg_replace( '/\.[^.]+$/', '', basename( $filename ) ),
+        'post_title'     => preg_replace( '/\.[^.]+$/', '', basename( $image_full_path ) ),
         'post_content'   => '',
     );
 
     // Insert the attachment.
-    $attach_id = wp_insert_attachment( $attachment, $filename, $parent_post_id );
+    $attach_id = wp_insert_attachment( $attachment, $image_full_path, $parent_post_id );
 
     // Make sure that this file is included, as wp_generate_attachment_metadata() depends on it.
     require_once( ABSPATH . 'wp-admin/includes/image.php' );
 
     // Generate the metadata for the attachment, and update the database record.
-    $attach_data = wp_generate_attachment_metadata( $attach_id, $filename );
+    $attach_data = wp_generate_attachment_metadata( $attach_id, $image_full_path );
     wp_update_attachment_metadata( $attach_id, $attach_data );
 
     set_post_thumbnail( $parent_post_id, $attach_id );
-
 }
