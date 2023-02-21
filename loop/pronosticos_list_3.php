075d651105cb42d4e3ac675954e3bf8e5058e65f
diaz web app <diazwebapp@gmail.com>
2023-02-21 12:44:54 -0400

    terminar vistas parley

--- loop/pronosticos_list_3.php
+++ loop/pronosticos_list_3.php
@@ -1,5 +1,5 @@
 <?php
-$current_user = get_current_user() ;
+$current_user = wp_get_current_user(  ) ;
 $params = get_query_var('params');
 $vip = carbon_get_post_meta($args["forecast"]->ID, 'vip');
 $permalink = get_the_permalink($args["forecast"]->ID);
