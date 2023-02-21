a9d5d06127e7c93edbbd0e04142f7f0f04a08313
diaz web app <diazwebapp@gmail.com>
2023-02-21 12:52:56 -0400

    terminar vistas parley

--- includes/shortcodes/shortcode-forecasts.php
+++ includes/shortcodes/shortcode-forecasts.php
@@ -14,7 +14,9 @@ function shortcode_forecast($atts)
         'title' => null
         
     ), $atts));
-    
+    var_dump(get_current_user_id(  ));
+    var_dump(get_currentuserinfo(  ));
+    var_dump(wp_get_current_user(  ));
     global $post;
     
     if ( is_a( $post, 'WP_Post' ) && has_shortcode( $post->post_content, 'forecasts' ) ) {
