151ff7df9b86b2ea77bdcbb7ef5967d7b0b8522b
diaz web app <diazwebapp@gmail.com>
2023-02-21 12:27:09 -0400

    terminar vistas parley

--- loop/pronosticos_list_3.php
+++ loop/pronosticos_list_3.php
@@ -132,49 +132,49 @@ if ($teams['team1']['logo'] and $teams['team2']['logo']):
     ;
     $html_vip_unloked =  "<div class='col-md-6 mt_30'>
                 
-                    <div class='event2_box event2_box3'>
-                    
-                            <div class='event2_top_box_wrapper'>
-                                <div class='event2_top_box'>
-                                    <div class='event_top_left'>
-                                        <img src='{$teams['team1']['logo']}' alt='{$teams['team1']['name']}' title='{$teams['team1']['name']}' class='img-fluid'>
-                                    </div>
-                                    <div class='event_top_middle'>
-                                        <p class='p1 {$sport['class']}'><b>". strtoupper($sport['name']) ."</b></p>
-                                            $time_format_html                                   
-                                        <p class='p2'><time datetime='".$date->format('Y-m-d h:i')."'>".date_i18n('d M', strtotime($date->format('Y-m-d')))."</time></p>                                    
-                                    </div>
-                                    <div class='event_top_right'>
-                                        <img src='{$teams['team2']['logo']}' alt='{$teams['team2']['name']}' title='{$teams['team2']['name']}' class='img-fluid' >
-                                    </div>
+                    <div class='event2_box'>
+                                
+                        <div class='event2_top_box_wrapper'>
+                            <div class='event2_top_box'>
+                                <div class='event_top_left'>
+                                    <img src='{$teams['team1']['logo']}' alt='{$teams['team1']['name']}' title='{$teams['team1']['name']}' class='img-fluid'>
                                 </div>
-                                <p class='p3' >
-                                    <h2><a href='$permalink'>                               
-                                    {$teams['team1']['name']} vs {$teams['team2']['name']}                              
-                                    </a> </h2>
-                                </p>
-                            </div>
-                        
-                            <div class='event2_box_middle_content event2_box3'>
-                                <p class='p1'>Pronóstico:</p>
-                                {$html_predictions}
-                                <div class='event2_box_bonus'>
-                                    <p class='p2'>Bonus:</p>
-                                    <p class='p3'>{$bookmaker['bonus_slogan']}</p>
+                                <div class='event_top_middle'>
+                                    <p class='p1 {$sport['class']}'><b>". strtoupper($sport['name']) ."</b></p>
+                                        $time_format_html                                   
+                                    <p class='p2'><time datetime='".$date->format('Y-m-d h:i')."'>".date_i18n('d M', strtotime($date->format('Y-m-d')))."</time></p>                                    
                                 </div>
-                                <div class='event_btn_box'>
-                                    <div class='event_btn_img'>
-                                        <a href='{$bookmaker['ref_link']}'>
-                                        <img src='{$bookmaker['logo']}' class='img-fluid' width='80' height='20' alt=''>
-                                        </a>
-                                    </div>
-                                    <div >
-                                        <a href='{$bookmaker['ref_link']}' class='button-ev2' rel='nofollow noopener noreferrer'>Juega ahora</a>
-                                    </div>
+                                <div class='event_top_right'>
+                                    <img src='{$teams['team2']['logo']}' alt='{$teams['team2']['name']}' title='{$teams['team2']['name']}' class='img-fluid' >
                                 </div>
                             </div>
+                            <p class='p3' >
+                                <h2><a href='$permalink'>                               
+                                {$teams['team1']['name']} vs {$teams['team2']['name']}                              
+                                </a> </h2>
+                            </p>
+                        </div>
+
+                        <div class='event2_box_middle_content'>
+                            <p class='p1'>Pronóstico:</p>
+                            {$html_predictions}
+                            <div class='event2_box_bonus'>
+                                <p class='p2'>Bonus:</p>
+                                <p class='p3'>{$bookmaker['bonus_slogan']}</p>
+                            </div>
+                            <div class='event_btn_box'>
+                                <div class='event_btn_img'>
+                                    <a href='{$bookmaker['ref_link']}'>
+                                    <img src='{$bookmaker['logo']}' class='img-fluid' width='80' height='20' alt=''>
+                                    </a>
+                                </div>
+                                <div >
+                                    <a href='{$bookmaker['ref_link']}' class='button-ev2' rel='nofollow noopener noreferrer'>Juega ahora</a>
+                                </div>
+                            </div>
+                        </div>
+
                     
-                        
                         <div class='panel-group' id='accordion' role='tablist' aria-multiselectable='false'>
                             <div class='panel panel-default'>
                             <div class='panel-heading accor_btn' role='tab' id='headingOne'>
