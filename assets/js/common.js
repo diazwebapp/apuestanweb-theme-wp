a1db0a3f439400fd9cc0c117c56de19fbbe9ae68
diaz web app <diazwebapp@gmail.com>
2023-02-27 16:12:48 -0400

    load more modificado

--- assets/js/common.js
+++ assets/js/common.js
@@ -192,6 +192,7 @@ async function load_more_items(e){
     params += forecasts_fetch_vars.timezone ? "&timezone="+forecasts_fetch_vars.timezone:"";
     params += forecasts_fetch_vars.text_vip_link ? "&text_vip_link="+forecasts_fetch_vars.text_vip_link:"";
     params += forecasts_fetch_vars.unlock ? "&unlock="+forecasts_fetch_vars.unlock:"";
+    params += forecasts_fetch_vars.current_user_id ? "&current_user_id="+forecasts_fetch_vars.current_user_id:"";
     params += "&odds="+forecasts_fetch_vars.odds;
     
     const request = await fetch(forecasts_fetch_vars.rest_uri+params)
