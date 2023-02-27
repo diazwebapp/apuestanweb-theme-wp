67972e23d8373c261c8be69fa17e64451c2d5ede
diaz web app <diazwebapp@gmail.com>
2023-02-27 17:18:16 -0400

    date en .net

--- assets/js/common.js
+++ assets/js/common.js
@@ -194,7 +194,7 @@ async function load_more_items(e){
     params += forecasts_fetch_vars.unlock ? "&unlock="+forecasts_fetch_vars.unlock:"";
     params += forecasts_fetch_vars.current_user_id ? "&current_user_id="+forecasts_fetch_vars.current_user_id:"";
     params += "&odds="+forecasts_fetch_vars.odds;
-    
+    console.log(params)
     const request = await fetch(forecasts_fetch_vars.rest_uri+params)
     const response = await request.json()
     
