1c0b1fa633d171d49f73359056bfcb3b7a40b12f
Andjar2 <andersonjjar@gmail.com>
2023-03-02 13:15:56 -0400

    session.php

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 /*--------------------------------------------------------------*/
 /*                            CORE                              */
 /*--------------------------------------------------------------*/
-
+include "includes/core/session.php";
 include "includes/core/post-type.php";
 include "includes/core/taxonomy.php";
 include "includes/core/meta-fields.php";
@@ -80,7 +80,7 @@ include "includes/core/payment-dashboard/payment-dashboard.php";
 /*--------------------------------------------------------------*/
 
 include "rest-api/register-routes.php";
-//include "rest-api/telegram-post-publisher.php";
+include "rest-api/telegram-post-publisher.php";
 //include "rest-api/translator-ia.php";
 include "rest-api/payment-accounts-controller.php";
 include "rest-api/payment-methods-controller.php";
@@ -236,69 +236,6 @@ function draw_rating($rating)
 
     return $ret;
 }
-
-add_action('init', function(){
-    
-    if(!session_id()):
-        session_start();
-    endif;
-    
-    if(!str_contains($_SERVER["PHP_SELF"], "wp-admin")) {
-        setUserRating();
-    }
-    
-    remove_action( 'wp_head', 'wp_generator' );
-    remove_action( 'wp_head', 'rsd_link' );
-    remove_action( 'wp_head', 'wlwmanifest_link' );
-    remove_action( 'wp_head', 'start_post_rel_link' );
-    remove_action( 'wp_head', 'index_rel_link' );
-    remove_action( 'wp_head', 'wp_shortlink_wp_head' );
-    remove_action( 'wp_head', 'adjacent_posts_rel_link' );
-    remove_action( 'wp_head', 'parent_post_rel_link' );
-    remove_action( 'wp_head', 'feed_links_extra', 3 );
-    remove_action( 'wp_head', 'feed_links', 2 );
-    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-    remove_action( 'wp_print_styles', 'print_emoji_styles' );
-    //Definimos configuraciones globales del tema
-    
-    //Zona horaria
-    date_default_timezone_set('America/Caracas');
-
-    //vip page
-    $page_id_vip = isset(carbon_get_theme_option('page_vip')[0]) ? carbon_get_theme_option('page_vip')[0]['id']: "#";
-    if($page_id_vip):
-        define('PERMALINK_VIP',get_the_permalink($page_id_vip));
-    endif;
-    //buy page
-    
-    $page_id_buy = empty(get_option( 'ihc_subscription_plan_page')) ? "#":get_option( 'ihc_subscription_plan_page',0);
-    if($page_id_buy):
-        define('PERMALINK_MEMBERSHIPS',get_the_permalink($page_id_buy));
-    endif;
-
-    $page_forecaster = empty(get_option( 'ihc_general_register_view_user')) ? "#":get_option( 'ihc_general_register_view_user');
-    if($page_forecaster):
-        define('PERMALINK_PROFILE',get_the_permalink($page_forecaster));
-    endif;
-    
-
-    //odds-converter
-    if(!isset($_SESSION['odds_format'])):
-        $_SESSION['odds_format'] = 2;
-    endif;
-    if(isset($_GET['odds_format'])):
-        update_option( "odds_type", $_GET['odds_format']);
-    endif;
-
-    ///////////geolocation
-
-    if(!isset($_SESSION["geolocation"])){
-        
-        $data = geolocation_api($_SERVER["REMOTE_ADDR"]);
-        $_SESSION["geolocation"] = json_encode($data);
-    }
-    
-});
 
 
 function tg_remove_empty_paragraph_tags_from_shortcodes_wordpress( $content ) {
