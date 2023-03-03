21d179d5981bf5a4001f866ee3c66e84cd27adc7
diaz web app <diazwebapp@gmail.com>
2023-03-03 00:27:47 -0400

    modificando templates emails

--- includes/templates-emails/template-email-2.php
+++ includes/templates-emails/template-email-2.php
@@ -17,10 +17,11 @@ function aw_email_templates_2($params=["blogname"=>"","username"=>"","vip_link"=
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <meta name="x-apple-disable-message-reformatting">
             <!--[if !mso]><!--><meta http-equiv="X-UA-Compatible" content="IE=edge"><!--<![endif]-->
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
             <title>{blogname}</title>    
         </head>
     <body>
-            <table class="" >
+            <table class="mx-auto table table-striped-columns" >
                 <thead>
                     <tr>
                         <th>
