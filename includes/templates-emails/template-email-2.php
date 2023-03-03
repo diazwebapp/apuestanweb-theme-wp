d0367e9050a29eaa8ce3ef3b04eb771cb6612f55
diaz web app <diazwebapp@gmail.com>
2023-03-03 00:31:41 -0400

    modificando templates emails

--- includes/templates-emails/template-email-2.php
+++ includes/templates-emails/template-email-2.php
@@ -5,23 +5,15 @@ function aw_email_templates_2($params=["blogname"=>"","username"=>"","vip_link"=
     $html = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
     <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
         <head>
-            <!--[if gte mso 9]>
-            <xml>
-            <o:OfficeDocumentSettings>
-                <o:AllowPNG/>
-                <o:AllowSVG/>
-            </o:OfficeDocumentSettings>
-            </xml>
-            <![endif]-->
-            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta name="x-apple-disable-message-reformatting">
-            <!--[if !mso]><!--><meta http-equiv="X-UA-Compatible" content="IE=edge"><!--<![endif]-->
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-            <title>{blogname}</title>    
+            <title>{blogname}</title> 
+            <style>
+                table{
+                    margin:auto !important;
+                }
+            </style>   
         </head>
     <body>
-            <table class="mx-auto table table-striped-columns" >
+            <table >
                 <thead>
                     <tr>
                         <th>
