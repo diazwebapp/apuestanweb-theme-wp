e27378c5110b2a00c3761defa93e1203998a463a
diaz web app <diazwebapp@gmail.com>
2023-03-04 13:10:28 -0400

    tema azul a email

--- includes/templates-emails/settings.php
+++ includes/templates-emails/settings.php
@@ -1,6 +1,18 @@
 <?php
-if(isset($_GET["message"])){
-    update_option( "email-bienvenida", $_GET["message"] );
+if(isset($_GET["registred"])){
+    update_option( "email-registred", $_GET["registred"]);
+    header("location:".$_SERVER["HTTP_REFERER"]);
+}
+if(isset($_GET["completed"])){
+    update_option( "email-completed", $_GET["completed"]);
+    header("location:".$_SERVER["HTTP_REFERER"]);
+}
+if(isset($_GET["pending"])){
+    update_option( "email-pending", $_GET["pending"]);
+    header("location:".$_SERVER["HTTP_REFERER"]);
+}
+if(isset($_GET["failed"])){
+    update_option( "email-failed", $_GET["failed"]);
     header("location:".$_SERVER["HTTP_REFERER"]);
 }
 /**
@@ -21,24 +33,71 @@ function books_register_ref_page() {
  * Display callback for the submenu page.
  */
 function vista_configuracion_emails() { 
-    $settings = array(
+    $registred = array(
         'teeny' => true,
-        'textarea_name' => 'message'
+        'textarea_name' => 'registred'
+    );
+    $completed = array(
+        'teeny' => true,
+        'textarea_name' => 'completed'
+    );
+    $pending = array(
+        'teeny' => true,
+        'textarea_name' => 'pending'
+    );
+    $failed = array(
+        'teeny' => true,
+        'textarea_name' => 'failed'
     );
     
-    //completed pending failed
     ?>
     <div class="wrap">
         <h1><?php _e( 'Books Personalización de emails', 'textdomain' ); ?></h1>
         <div class="container">
-            <form action="">
-                <label for="">Mensaje Reqgistro</label>
-                <div class="card mx-auto">
+            <div class="row">
+                <div class="col-md-6">
+                    <form action="">
+                        <label for="">Mensaje Reqgistro</label>
+                        <div class="card mx-auto">
 
-                    <?php wp_editor( get_option("email-bienvenida"),"email", $settings ); ?>
+                            <?php wp_editor( get_option("email-registred"), $registred ); ?>
+                        </div>
+                        <input type="submit" value="guardar">
+                    </form>
                 </div>
-                <input type="submit" value="guardar">
-            </form>
+
+                <div class="col-md-6">
+                    <form action="">
+                        <label for="">Mensaje pago completado</label>
+                        <div class="card mx-auto">
+
+                            <?php wp_editor( get_option("email-pago-completed"), $completed ); ?>
+                        </div>
+                        <input type="submit" value="guardar">
+                    </form>
+                </div>
+                <div class="col-md-6">
+                    <form action="">
+                        <label for="">Mensaje pago pendiente</label>
+                        <div class="card mx-auto">
+
+                            <?php wp_editor( get_option("email-pago-pending"), $pending ); ?>
+                        </div>
+                        <input type="submit" value="guardar">
+                    </form>
+                </div>
+                <div class="col-md-6">
+                    <form action="">
+                        <label for="">Mensaje pago fallido</label>
+                        <div class="card mx-auto">
+
+                            <?php wp_editor( get_option("email-pago-failed"), $failed ); ?>
+                        </div>
+                        <input type="submit" value="guardar">
+                    </form>
+                </div>
+            </div>
+            
         </div>
         <?php echo get_option("email-bienvenida") ?>
     </div>
