5545b3a8edd985d16726cbc707ce4446b59cf916
Andjar2 <andersonjjar@gmail.com>
2023-03-06 20:38:02 -0400

    dev-full backgrounds 2

--- includes/shortcodes/shortcode-predictions.php
+++ includes/shortcodes/shortcode-predictions.php
@@ -55,7 +55,7 @@ endif;
 
                 $ret .= "<div class='single_event_match_title_box'>
                             <div class='tip-p row mx-1 px-4 py-4'>
-                            <a href='{$bookmaker['ref_link']}' rel='nofollow'><img width='80' height='20' loading='lazy' src='{$bookmaker['logo_2x1']}'  style='background:{$bookmaker['background_color']};border-radius: 6px;padding: 4px;height: 3rem;' alt='{$bookmaker['name']}'></a>
+                            <a href='{$bookmaker['ref_link']}' rel='nofollow noreferrer noopener' target='_blank'><img width='80' height='20' loading='lazy' src='{$bookmaker['logo_2x1']}'  style='background:{$bookmaker['background_color']};border-radius: 6px;padding: 4px;height: 3rem;' alt='{$bookmaker['name']}'></a>
                             </div>
                             <table class='table-single-predict'>
                             <thead>
@@ -73,7 +73,7 @@ endif;
                                 </tr>
                             </tbody>
                             </table>
-                            <a href='{$bookmaker['ref_link']}' rel='nofollow'><button id='event-button'>Apostar</button></a>
+                            <a href='{$bookmaker['ref_link']}' rel='nofollow noreferrer noopener' target='_blank'><button id='event-button'>Apostar</button></a>
                             
                         </div>
                         <div class='d-flex justify-content-center'>
