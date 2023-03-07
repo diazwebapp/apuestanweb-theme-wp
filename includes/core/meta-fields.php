525e6b8f88deb8aaa83c7154403bba86ff23636b
diaz web app <diazwebapp@gmail.com>
2023-03-06 23:11:43 -0400

    empezando tareas

--- includes/core/meta-fields.php
+++ includes/core/meta-fields.php
@@ -113,7 +113,7 @@ function crb_attach_theme_options()
                     Field::make('checkbox', 'disable_table', __("Desactivar tabla de contenidos", "jbetting")),
                     Field::make('text', 'ref', __("Refferal link", "jbetting")),
                     
-                    Field::make( 'html', 'rating', __("Rating(1,2,3,4,5)", "jbetting"))
+                    Field::make( 'html', 'calificacion', __("Rating(1,2,3,4,5)", "jbetting"))
                         ->set_html( '<input type="range" min="5" max="10" step="0.01">' ),
                     Field::make('text', 'bonus_slogan', __("Bonus slogan", "jbetting")),
                     Field::make('text', 'bonus_amount', __("Bonus ammount", "jbetting")),
