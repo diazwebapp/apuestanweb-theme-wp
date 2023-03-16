d149702e8732090dd4f696659c7c327d2e557521
diaz web app <diazwebapp@gmail.com>
2023-03-15 23:09:23 -0400

    mejorando get_bookmaker_payments

--- includes/handlers/get_bookmaker_by_post.php
+++ includes/handlers/get_bookmaker_by_post.php
@@ -32,7 +32,7 @@ function get_bookmaker_payments($bookmaker_id){
         
         foreach($methods as $key_item => $item){
             echo "<pre>";
-            var_dump($item);
+            var_dump($item['payment_method']);
             echo "</pre>";
             $term = get_term($item['payment_method'][0]["id"],$item['payment_method'][0]["subtype"]);
             
