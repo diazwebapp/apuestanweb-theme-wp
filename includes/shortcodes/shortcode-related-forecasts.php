dfdd66d1f95a7d19239bdc51a107d25bb36571b4
diaz web app <diazwebapp@gmail.com>
2023-03-17 23:45:04 -0400

    cambiando metodo de las estadisticas de pronosticadores

--- includes/shortcodes/shortcode-related-forecasts.php
+++ includes/shortcodes/shortcode-related-forecasts.php
@@ -1,29 +1,30 @@
 <?php
 
-function shortcode_related_forecast($atts)
+function shortcode_forecastrelated_($atts)
 {
     extract(shortcode_atts(array(
         'num' => 6,
-        'league' => null,
+        'league' => wp_get_post_terms(get_the_ID(), 'league', array('field' => 'slug')),
         'date' => null,
         'model' => 1,
         'text_vip_link' => 'VIP',
         'filter' => null,
         'time_format' => null,
         'paginate' => null,
-        'page_title' => null,
         'title' => null
+        
     ), $atts));
     global $post;
-    if ( is_a( $post, 'WP_Post' ) && has_shortcode( $post->post_content, 'related-forecasts' ) ) {
+    
+    if ( is_a( $post, 'WP_Post' ) && has_shortcode( $post->post_content, 'forecasts' ) ) {
         wp_enqueue_style( 's-forecasts-css', get_template_directory_uri( ) .'/assets/css/forecasts-styles.css', null, false, 'all' );
     }else if(is_single(  )){
         wp_enqueue_style( 's-forecasts-css', get_template_directory_uri( ) .'/assets/css/forecasts-styles.css', null, false, 'all' );
     }
-    
     $ret = "";
 
     $geolocation = json_decode($_SESSION["geolocation"]);
+    
     $odds = get_option( 'odds_type' );
     
     if(is_page() && !$title)
@@ -39,20 +40,23 @@ function shortcode_related_forecast($atts)
     $title = empty($custom_h1) ? $title : $custom_h1;
 
     if($filter):
-        $sport_url = '#';
-        if(isset($league) or isset($page_title)):
-            $sport_page = get_page_by_title($page_title);
-            $sport_url = isset($sport_page->ID) ? get_permalink($sport_page->ID) : get_term_link($league, 'league');
-        endif;
-
-        $ret .= '<div class="title_wrap single_event_title_wrap">
-                    <h3 class="title-b mt_30 order-lg-1">'.(isset($title) ? $title : '').'</h3>
-                    <a href="'.$sport_url.'" class="mt_30 dropd order-lg-3">Ver Todo</a>
-                </div>';
-        
+        $ret .= "<div class='row'>                    
+                    <h1 class='title col-8'>".(isset($title) ? $title : '')."</h1>
+                    
+                    <div class='col-4 justify-content-end d-flex event_select'>
+                        <select name='ord' data-type='forecast' id='element_select_forecasts' onchange='filter_date_items(this)'>
+                            <option value='' ".( !$date ? 'selected' : '').">".__('Todo','jbetting')."</option>
+                            <option value='ayer' ".( $date == 'ayer' ? 'selected' : '')." > ".__('Ayer','jbetting')." </option>
+                            <option value='hoy' ".( $date == 'hoy' ? 'selected' : '')." >".__('Hoy','jbetting')." </option>
+                            <option value='mañana' ".( $date == 'mañana' ? 'selected' : '')." > ".__('Mañana','jbetting')." </option>
+                        </select>
+                    </div>
+                    
+                </div>";
     endif;
     
-    $args = [];
+    
+
     $league_arr = null;
     
     if(is_array($league) and count($league) > 0):
@@ -69,8 +73,8 @@ function shortcode_related_forecast($atts)
         $league_arr = "[{replace-leagues}]";
         $league_arr = str_replace("{replace-leagues}",$league,$league_arr);
     endif;
-        
-    //$query = new WP_Query($args);
+   
+    $args = [];
     $args['paged']          = ( get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1);
     $args['posts_per_page'] = $num;
     $args['leagues'] =  $league_arr;
@@ -81,10 +85,13 @@ function shortcode_related_forecast($atts)
     $args['rest_uri'] = get_rest_url(null,'aw-forecasts/forecasts');
     $args['country_code'] = $geolocation->country_code;
     $args['timezone'] = $geolocation->timezone;
-    $args["current_user_id"] =  get_current_user_id();
     $args['odds'] = $odds;
     $args['exclude_post'] = null;
-    $args['btn_load_more'] = "<button onclick='load_more_items(this)' data-type='forecast' id='load_more_forecast' class='loadbtn btn d-flex justify-content-center mt-5'> ".__( 'Cargar más', 'jbetting' ) ."</button><br/>";
+    $args["current_user_id"] =  get_current_user_id();
+    $args['btn_load_more'] = "<button onclick='load_more_items(this)' data-page='{$args['paged']}' data-type='forecast' id='load_more_forecast' class='loadbtn btn d-flex justify-content-center mt-5'> 
+    
+    ".__( 'Cargar más', 'jbetting' ) ."
+    </button><br/>";
     
     $post_type = get_post_type( );
     if($post_type == "forecast" and is_single()):
@@ -102,23 +109,23 @@ function shortcode_related_forecast($atts)
     $params .= isset($args['timezone']) ? "&timezone={$args['timezone']}":"";
     $params .= isset($args['exclude_post']) ? "&exclude_post={$args['exclude_post']}":"";
     $params .= "&current_user_id={$args['current_user_id']}";
-
     $params .= "&odds=$odds";
+    wp_add_inline_script( 'common-js', "let forecasts_fetch_vars = ". json_encode($args) );
     
     $response = wp_remote_get($args['rest_uri'].$params,array('timeout'=>10));
     
     $query =  wp_remote_retrieve_body( $response );
-    
-    if ($query) {
+    if (!is_wp_error( $query )) {
         $home_class = "event_wrap pt_30";
             if($model and $model != 1)
                 $home_class = 'row';        
         
         $loop_html = ''; 
-        $ret .="<div class='$home_class' style='align-items:baseline;' id='games_list' >{replace_loop}</div>";
+        $ret .="<div class='$home_class' id='games_list' >{replace_loop}</div>";
         $data_json = json_decode($query);
         
         $loop_html = $data_json->html;
+        
         $ret = str_replace("{replace_loop}",$loop_html,$ret);
         
     } else {
