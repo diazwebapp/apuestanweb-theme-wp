d864cd116a13f553afadd90a8c83fa42ad641055
diaz web app <diazwebapp@gmail.com>
2023-03-30 08:00:14 -0400

    a ver si no se rompe na

--- rest-api/forecaster-controller.php
+++ rest-api/forecaster-controller.php
@@ -5,12 +5,12 @@ function aw_get_forecaster_data(WP_REST_Request $request){
     $author_id = isset($params["author_id"]) ? $params["author_id"] : 1;
     $paged = isset($params["paged"]) ? $params["paged"] : 1;
     $meta_key = isset($params["vip"]) ? "vip" : "free";
-    
+
     wp_reset_postdata();
     $args['post_type'] = $params["post_type"];
     $args['author'] = $author_id;
     $args['paged'] = $paged;
-    $args['posts_per_page'] = 3;
+    $args['posts_per_page'] = 9;
 
     if($meta_key == 'vip'):
         $args['meta_query'] = [
