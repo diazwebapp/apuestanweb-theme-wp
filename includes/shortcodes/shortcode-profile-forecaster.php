4cb58800bd0d0e5f6ef63d74def559478eea738e
diaz web app <diazwebapp@gmail.com>
2023-03-30 07:57:51 -0400

    a ver si no se rompe na

--- includes/shortcodes/shortcode-profile-forecaster.php
+++ includes/shortcodes/shortcode-profile-forecaster.php
@@ -31,7 +31,7 @@ function shortcode_profile_forecaster($atts)
             ;
         $a_elements = '';
         var_dump($data_json_vip->max_pages);
-        if(intval($data_json_vip->max_pages) > 1):
+        if(intval($data_json_vip->max_pages) > 0):
             for($i=0;$i<=intval($data_json_vip->max_pages);$i++):
                 $a_elements .= '<a class="page-numbers '.($args['paged_vip'] == $i ? 'current' : '').'" href="?profile='.$args["author_id"].'&page_vip='.($i+1).'">'.($i+1).'</a>';
             endfor;
