0c56750538cc2a0360a8ac017b049901daaf1ccb
diazwebapp <diazwebapp@gmail.com>
2024-12-04 09:53:06 -0400

    mejorando rendimiento

--- includes/handlers/blog_posts_table.php
+++ includes/handlers/blog_posts_table.php
@@ -8,7 +8,7 @@ function aw_blog_posts_table($items){
 	// field names
 	while ($items->have_posts()) :
         $items->the_post();
-        $thumb = get_the_post_thumbnail_url(get_the_ID(),'120x70');
+        $thumb = get_the_post_thumbnail_url(get_the_ID(),'medium');
         $leagues = wp_get_post_terms(get_the_ID(), 'league', array('fields' => 'all'));
         $sport = '';
         
