44da11ce7697dfb98fad14039199f496578fc9a9
diazwebapp <diazwebapp@gmail.com>
2024-12-04 13:35:29 -0400

    mejorando rendimiento

--- includes/core/custom-field-forecast.php
+++ includes/core/custom-field-forecast.php
@@ -14,8 +14,25 @@ function aw_forecast_imagen_destacada_personalizada() {
     }  */
     $template = '<div style="position:relative !important;width:854px;max-width:854px;height:480px;top:0;left:0;background:red;" id="thumb-template">
     <img src="{replacebg}" width="854" height="480" id="plantilla" style="height:480px;"/>
-    <img src="{replace-team-1}" width="130px" height="130px" class="d-none img-fluid" style="position: absolute;top: 36.8%;left: 16.4%;width: 130px;max-height:130px;height:130px;object-fit:contain;" id="equipo-1" />
-    <img src="{replace-team-1}" width="130px" height="130px" class="d-none img-fluid" style="position: absolute;top: 36.8%;left: 68.5%;width: 130px;max-height:130px;height:130px;object-fit:contain;" id="equipo-2" />
+    <img src="{replace-team-1}" width="130px" height="130px" class="d-none img-fluid" style="
+                            position: absolute;
+                            top: 37.8%;
+                            left: 19.3%;
+                            background: rgba(0,0,0, .3);
+                            width: 14.06%;
+                            height: 25%;
+                            object-fit:contain;
+
+                    "  id="equipo-1" />
+    <img src="{replace-team-1}" width="130px" height="130px" class="d-none img-fluid" style="
+                            position: absolute;
+                            top: 37.8%;
+                            left: 66.6%;
+                            background: rgba(0,0,0, .3);
+                            width: 14.06%;
+                            height: 25%;
+                            object-fit:contain;"
+                    id="equipo-2" />
 
     
 </div>';
@@ -38,24 +55,25 @@ function aw_forecast_imagen_destacada_personalizada() {
                         <button class="btn btn-outline-light" type="button" style="position:absolute;top:5px;left:5px;background:rgba(0,0,0, .3);" target-html-id="plantilla" onclick="aw_set_imgs(this)">reemplazar plantilla <i class="dashicons dashicons-edit" ></i></button>
 
                         <div role="button" class="d-flex justify-content-center align-items-center text-light" style="
-                                        position: absolute;
-                                        top: 36.8%;
-                                        left: 16.4%;
-                                        background: rgba(0,0,0, .3);
-                                        width: 130px;
-                                        height: 130px;
+                            position: absolute;
+                            top: 37.8%;
+                            left: 19.3%;
+                            background: rgba(0,0,0, .3);
+                            width: 14.06%;
+                            height: 25%;
+
                                     " 
                         target-html-id="equipo-1" onclick="aw_set_imgs(this)">
                             <i class="dashicons dashicons-edit" ></i>
                         </div>
 
                         <div type="button" class="d-flex justify-content-center align-items-center text-light" style="
-                                        position: absolute;
-                                        top: 36.8%;
-                                        left: 68.5%;
-                                        background: rgba(0,0,0, .3);
-                                        width: 130px;
-                                        height: 130px;
+                            position: absolute;
+                            top: 37.8%;
+                            left: 66.6%;
+                            background: rgba(0,0,0, .3);
+                            width: 14.06%;
+                            height: 25%;
                                     " 
                         target-html-id="equipo-2" onclick="aw_set_imgs(this)">
                             <i class="dashicons dashicons-edit" ></i>
