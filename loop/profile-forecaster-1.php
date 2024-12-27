6f8be5fb16d4ce440e5fb08a77ac5fedf74beef4
diazwebapp <diazwebapp@gmail.com>
2024-12-27 15:49:48 -0400

    modificando forecaster

--- loop/profile-forecaster-1.php
+++ loop/profile-forecaster-1.php
@@ -157,7 +157,7 @@ if(isset($_GET['profile'])):
     <div class="free__tab">
         <nav>
             <div class="free-tabs nav " id="nav-tab" role="tablist">
-                <a class="free-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">ApuestanPlus</a>
+                <a class="free-link '.(isset($_GET['page_vip']) ? "active": "").'" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">ApuestanPlus</a>
                 <a class="free-link '.(isset($_GET['page_free']) ? "active": "").'" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Free</a>
                 <a class="free-link '.(isset($_GET['page_posts']) ? "active": "").'" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Articulos</a>
             </div>
