0f7d70090cf61baa2f0cb545da19a2df6e9e21d7
diazwebapp <diazwebapp@gmail.com>
2025-01-05 11:14:22 -0400

    modificando blog

--- loop/posts-grid_3.php
+++ loop/posts-grid_3.php
@@ -15,7 +15,7 @@ if(count($leagues) > 0):
 endif;
 echo "<div class='col-6 col-lg-4 mt-5'>
     <div class='notis_box'>
-    <img class='img-fluid' alt='$title' width='230' height='153' loading='lazy' src='$thumbnail_url' alt='teamvs'>
+    <img alt='$title' width='230' height='153' loading='lazy' src='$thumbnail_url' alt='teamvs'>
         <a href='$permalink'>
             <p class='text-white'>$title</p>
         </a>
