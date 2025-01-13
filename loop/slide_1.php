3e141f23d0881b7e5a0395610f4a87b5fb3bd4ee
diazwebapp <diazwebapp@gmail.com>
2025-01-13 12:55:02 -0400

    reescribiendo el css

--- loop/slide_1.php
+++ loop/slide_1.php
@@ -69,45 +69,49 @@ if (!$p2) {
 echo "<div class='owl-item' >
         <div class='item' style='background-image:linear-gradient(145deg,#03b0f4 0,#051421c4 50%,#dc213e 100%),url( $slide_bg ); background-repeat:no-repeat;background-size:cover;'>
             <div class='row align-items-center'>
-                <div class='col-lg-7'>
-                    <p class='text-center text-muted mb-4' >Enfrentamiento</p>
+                <div class='col-lg-8'>
                     <div class='row'>
+                        <div class='col-12 ml-slide'><p class='text-muted' >Enfrentamiento</p></div>
+
                         <div class='col-12 col-lg-4 d-flex align-items-center justify-content-center'>
-                            <div class='team_logo team_logo rounded-circle p-4 bg-light'>
+                            <div class='team_logo rounded-circle d-flex justify-content-center bg-light'>
                                 <img width='40px;' height='40px' src=' {$teams['team1']['logo']} ' class='img-fluid' alt=' {$teams['team1']['name']} '>
                             </div>
                             <div class='mx-5 d-lg-none d-block'>
                                 <img src='$cross_img' width='40px;' height='40px' alt='teamvs'>
                             </div>                                
-                            <div class='team_logo team_logo rounded-circle p-4 bg-light'>
+                            <div class='team_logo rounded-circle d-flex justify-content-center bg-light'>
                                 <img width='40px;' height='40px' src=' {$teams['team2']['logo']} ' class='img-fluid' alt=' {$teams['team2']['name']} '>
                             </div>
                         </div>
-                        <div class='col-12 col-lg-8 text-center my-4'>
-                            <span class='d-none d-sm-none d-md-block text-light team-name' > {$teams['team1']['name']} <strong class='text-light'>VS</strong>  {$teams['team2']['name']} </span>
+                        <div class='col-12 col-lg-8 my-4 team-name'>
+                            <span class='d-none d-sm-none d-md-block text-light' > {$teams['team1']['name']} <strong class='text-light'>VS</strong>  {$teams['team2']['name']} </span>
                             
-                            <span class='d-block d-sm-block d-md-none text-light text-uppercase team-name'> {$teams['team1']['acronimo']}rt <strong class='text-light'>VS</strong> y {$teams['team2']['acronimo']} </span>
+                            <span class='d-block d-sm-block d-md-none text-light text-uppercase'> {$teams['team1']['acronimo']}rt <strong class='text-light'>VS</strong> y {$teams['team2']['acronimo']} </span>
                             
                             <p class='text-light'>$fecha $hora</p>
                         </div>
+
+                        <div class='col-12 ml-slide'>
+                            <a href='$permalink' class='btn btn-outline-light btn-sm'>".__('Ver pronóstico','jbetting')."</a>
+                        </div>
                     </div>
-                    <div class='text-center'>
-                        <a href='$permalink' class='btn btn-outline-light btn-sm'>".__('Ver pronóstico','jbetting')."</a>
-                    </div>
+                    
                 </div>
-                <div class='col-lg-5 d-none d-lg-flex justify-content-end'>
+                
+                <div class='col-lg-4 d-none d-lg-flex justify-content-end'>
                     <div class='d-block'>
-                        <div class='team_box '>
+                        <div class='team_box px-3'>
                             <img width='60px' height='60px' src='{$teams['team1']['logo']}' class='img-fluid'  alt='{$teams['team1']['name']}'>
                             
-                                <b>vs</b>
+                                <b class='mx-3' >vs</b>
                             
                             <img width='60px' height='60px' src='{$teams['team2']['logo']}' class='img-fluid'  alt='{$teams['team2']['name']}'>
                         </div>                            
                         <div class='team_box py-1 mt-3 text-center'>
                             <b> $p1 </b>
                             
-                            <b> $x </b>
+                            <b class='mx-3'> $x </b>
                             
                             <b> $p2 </b>
                         </div>
